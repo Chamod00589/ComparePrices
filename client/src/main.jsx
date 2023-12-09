0b58251f0ed4d9c279be2efa85bd1b070331f256
Chamod00589 <chamodkavinda2013@gmail.com>
2023-12-09 11:31:51 +0530

    Add react-router-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,3 +8,22 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+
+
+
+
+
+// import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+// import './index.css';
+// import { persistor, store } from './redux/store.js';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <PersistGate loading={null} persistor={persistor}>
+//       <App />
+//     </PersistGate>
+//   </Provider>
+// );
