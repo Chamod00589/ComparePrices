8846115f34938076033941757228859219f530c1
Chamod00589 <chamodkavinda2013@gmail.com>
2024-02-04 23:41:50 +0530

    render.com test

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
 import express from "express";
-
 const app = express();
+
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
 app.listen(4000, () => {
   console.log("Server is running on port 3000!");
 });
+ 
