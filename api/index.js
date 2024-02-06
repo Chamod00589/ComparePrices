4f119000372711e9e85757a8570686d7042101f1
Chamod00589 <chamodkavinda2013@gmail.com>
2024-02-06 22:49:15 +0530

    kjsadb

--- api/index.js
+++ api/index.js
@@ -1,26 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "path";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-import pricingRouter  from "./routes/pricing.route.js";
+import pricingRouter from "./routes/pricing.route.js";
 
-
-import path from "path";
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "/client/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 
 
 const port = process.env.PORT || 4000;
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
 // http://localhost:3000/api/pricing/get/63e963bba6803e6331dd4762
 app.use("/api/pricing", pricingRouter);
@@ -30,12 +22,18 @@ mongoose
   .then(() => {
     console.log("Connected to MongoDB!");
   })
-  .catch((err) => { 
+  .catch((err) => {
     console.log(err);
   });
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
-  
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
